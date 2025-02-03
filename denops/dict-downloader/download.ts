38b928ca3afffea5c2a18a4db68dee84d5aa669b
kei-s16 <kei1216s@nifty.com>
2025-02-04 00:51:31 +0900

    とりあえず動くところまで

---
+++ denops/dict-downloader/download.ts
@@ -0,0 +1,58 @@
+import os from 'node:os';
+
+export class DictDownloader {
+    // TODO: add docs
+    async fetchDictionary(dictUrl: string, dictDir: string) {
+      const url = new URL(dictUrl);
+      const path = this.dictDirResolver(dictDir);
+      const dictName = this.dictNameResolver(url);
+
+      if (!dictName) {
+        return new Error("渡されたURLが不正"); // TODO: fix error
+      }
+
+      const dest = path.concat(dictName);
+
+      try {
+        const response = await fetch(url, {method: "GET"});
+
+        if (response.body) {
+          const savedFile = await Deno.open(dest, {write: true, create: true});
+          await response.body.pipeTo(savedFile.writable);
+        }
+
+        // 保存されたファイルがgzなら解凍する
+        const dictFile = await Deno.open(dest, {read: true});
+        if (dictFile && dest.endsWith(".gz")) { // TODO: ほんとはファイルタイプ見るべきなので直す
+          const unzipPath = await Deno.create(dest.slice(0, -3));
+          const unzipStream = unzipPath.writable;
+          const stream = new DecompressionStream("gzip");
+          dictFile.readable.pipeThrough(stream).pipeTo(unzipStream);
+        }
+
+      } catch (e) {
+        console.error(e.message);
+      }
+    };
+
+    // TODO: add docs
+    dictDirResolver(dictDir: string) {
+      const regex = /^~\//
+      const path = new String(dictDir).replace(regex, `${os.homedir()}/`);
+
+      if (!path.endsWith('/')) {
+        return path.concat("/");
+      }
+
+      return path;
+    };
+
+    // TODO: add docs
+    dictNameResolver(url: URL) : string|undefined {
+      return url.pathname.split("/").pop();
+    }
+
+    // TODO: add docs
+    unzip() {}
+}
+
---
+++ denops/dict-downloader/main.ts
@@ -0,0 +1,33 @@
+import type { Entrypoint } from "jsr:@denops/std@7.4.0";
+import * as vars from "jsr:@denops/std@7.4.0/variable";
+import { DictDownloader } from "./download.ts"
+
+export const main: Entrypoint = (denops) => {
+  const downloader = new DictDownloader;
+
+  denops.dispatcher = {
+    async init() {
+      await denops.cmd(
+        `command! -nargs=? DictDownload echomsg denops#request('${denops.name}', 'download', [<q-args>])`,
+      );
+    },
+
+    async download() {
+      // TODO: change var name
+      const dicts = await vars.globals.get(denops, "dicts") as Array<string>;
+      const savepath = await vars.globals.get(denops, "savepath") as string;
+
+      try {
+        dicts.forEach(async (dict) => {
+          await downloader.fetchDictionary(dict, savepath);
+        });
+
+        return `Downloaded.`;
+      } catch (e) {
+        console.error(e.message);
+      }
+    }
+  };
+}
+
+
